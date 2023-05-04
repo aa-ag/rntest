48e3790ae1fc006fb14b4ac5aefb58f2b371d0d8
aa-ag <aaron.aguerrevere@gmail.com>
2023-05-04 09:32:49 -0500

    Add onPress to TouchableWithoutFeedback

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableWithoutFeedback } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Testing Images</Text>
-      <Image style={{ width: 100, height: 80 }} source={require("./assets/icon.png")} />
+      <TouchableWithoutFeedback onPress={() => console.log("working!")}>
+        <Image style={{ width: 100, height: 80 }} source={require("./assets/icon.png")} />
+      </TouchableWithoutFeedback>
     </SafeAreaView>
   );
 }
