bd1c7a442921b083d4998b097e31227e33f91a76
aa-ag <aaron.aguerrevere@gmail.com>
2023-06-05 21:15:17 -0500

    Add styling

--- cars/App.js
+++ cars/App.js
@@ -2,10 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header';
 import Footer from './src/components/Footer';
-
-
+  
 export default function App() {
-  return
+  return (
+    <View
+      style={{
+        flex: 0.5,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "green",
+      }}
+    >
       <Header />
-      // <Footer />
+      <Text
+        style={{
+          color: "white",
+        }}>
+        test
+      </Text>
+    </View>
+  );
 }
